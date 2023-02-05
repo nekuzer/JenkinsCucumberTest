2faa8457306a18c0875beb2f36ecfcaca2d0e2a7
NikolayKhat <n.n.khatunitsky@mail.ru>
2023-02-06 00:58:42 +0300

    fix

--- src/test/java/steps/TestStep.java
+++ src/test/java/steps/TestStep.java
@@ -1,12 +1,17 @@
 package steps;
 
+import com.codeborne.selenide.Condition;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestStep {
 
     @When("Test")
     public void test() {
-        open("https://www.googlehkjhhkjhk.com/?client=safari");
+        open("https://www.google.com/?client=safari");
+        $(By.xpath("//div[contains(text(), 'sfesfesfsefsef')]")).shouldBe(Condition.visible.because("Его нет"));
     }
 }
