5ed52aaeaf69c265a09104bfc557ba9b165275dd
NikolayKhat <n.n.khatunitsky@mail.ru>
2023-02-07 01:13:56 +0300

    fix

--- src/test/java/steps/TestStep.java
+++ src/test/java/steps/TestStep.java
@@ -1,8 +1,6 @@
 package steps;
 
-import com.codeborne.selenide.Browser;
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.*;
 import io.cucumber.java.*;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
@@ -11,11 +9,13 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Base64;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestStep {
 
-    public static WebDriver driver = new ChromeDriver();
+//    public static WebDriver driver = new ChromeDriver();
 
     @Before
     public void before() {
@@ -25,23 +25,19 @@ public class TestStep {
     public void after(Scenario scenario) {
         if (scenario.isFailed()) {
             System.out.println("---------------- ok ----------------");
-            TakesScreenshot ts = (TakesScreenshot) driver;
-            byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
-
-            scenario.attach(screenshot, "image/png", "ok");
-
-        } else {
-            TakesScreenshot ts = (TakesScreenshot) driver;
-            byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot, "image/png", "not ok");
+//            TakesScreenshot ts = (TakesScreenshot) driver;
+//            byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
+//
+//            scenario.attach(screenshot, "image/png", "ok");
+            byte[] decoded = Base64.getDecoder().decode(Selenide.screenshot(OutputType.BASE64));
+            scenario.attach(decoded, "image/png", "ok");
         }
 
     }
 
     @When("Test")
     public void test() {
-        driver.get("https://www.google.com/?client=safari");
-        $(By.xpath("//div[contains(text(), '')]")).shouldBe(Condition.visible.because("Его нет"));
-        driver.close();
+        open("https://www.google.com/?client=safari");
+        $(By.xpath("//div[contains(text(), 'ddaawd')]")).shouldBe(Condition.visible.because("Его нет"));
     }
 }
--- src/test/resources/feature/1_Test1.feature
+++ src/test/resources/feature/1_Test1.feature
@@ -1,3 +1,6 @@
 Feature: Test
   Scenario: Test1
     When Test
+
+  Scenario: Test2
+    When Test
